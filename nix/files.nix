9574f76c71cf617ff4a161d57c9131f517b2faf2
Jeff Hykin <jeff.hykin@gmail.com>
2022-03-02 11:03:05 -0600

    cleanup files.nix

--- nix/files.nix
+++ nix/files.nix
@@ -1,24 +1,111 @@
-{ pkgs ? import <nixpkgs> { }, python, hdf5, mpi, zlib, cudatoolkit, cudnn, magma }:
+# 
+# setup
+# 
+let 
+    # 
+    # pick which frozen version of nixpkgs to default to
+    # 
+    # 7e9b0dff974c89e070da1ad85713ff3c20b0ca97 # <- alternative
+    pinnedNixVersion = "8917ffe7232e1e9db23ec9405248fd1944d0b36f"; # this is a hash of a specific commit
+    pinnedNix = (builtins.import 
+        (builtins.fetchTarball
+            ({
+                url=''https://github.com/NixOS/nixpkgs/archive/${pinnedNixVersion}.tar.gz'';
+            })
+        )
+        ({
+            config = {
+                allowUnfree = true;
+            };
+        })
+    );
+    
+    hdf5Drv = (pinnedNix.hdf5.overrideAttrs
+        (
+            oldHdf5: rec {
+                pname = "hdf5";
+                version = "1.12.0";
+                src = builtins.fetchurl {
+                    url="https://support.hdfgroup.org/ftp/HDF5/releases/hdf5-1.12/${pname}-${version}/src/${pname}-${version}.tar.bz2";
+                    sha256 = "0qazfslkqbmzg495jafpvqp0khws3jkxa0z7rph9qvhacil6544p";
+                };
+            }
+        )
+    );
 
-{
-  base-env-vars = pkgs.writeText "base-env-vars" ''
-    # Keep track of project directory
-    export PROJECT_DIR=$(pwd)
+    hdf5WithOverrides = (hdf5Drv.override
+        {
+            mpiSupport         = true; 
+            stdenv             = pinnedNix.stdenv ;
+            fetchurl           = pinnedNix.fetchurl ;
+            removeReferencesTo = pinnedNix.removeReferencesTo ;
+            mpi                = pinnedNix.mpi ;
+            zlib               = pinnedNix.zlib;
+        }
+    );
+    
+    defaults = {
+        stdenv        = pinnedNix.stdenv;
+        hdf5          = hdf5WithOverrides;
+        python        = pinnedNix.python38;
+        mpi           = pinnedNix.mpi;
+        zlib          = pinnedNix.zlib;
+        libuv         = pinnedNix.libuv;
+        openmpi       = pinnedNix.openmpi;
+        magma         = pinnedNix.magma;
+        cudatoolkit   = pinnedNix.cudatoolkit_11_1;
+        cudnn         = pinnedNix.cudnn_cudatoolkit_11_1;
+        writeText     = pinnedNix.writeText;
+        writeTextFile = pinnedNix.writeTextFile;
+    };
+# 
+# actual function being exported
+# 
+in
+    args@{
+        stdenv      ? defaults.stdenv     ,
+        hdf5        ? defaults.hdf5       ,
+        python      ? defaults.python     ,
+        openmpi     ? defaults.openmpi    ,
+        zlib        ? defaults.zlib       ,
+        cudatoolkit ? defaults.cudatoolkit,
+        cudnn       ? defaults.cudnn      ,
+        magma       ? defaults.magma                
+    }:
+        let
+            # push the cuda toolkit into the other tools
+            argsWithOverrides = args // {
+                mpi   = args.openmpi.override {
+                    cudaSupport = true; 
+                    cudatoolkit = args.cudatoolkit;
+                };
+                magma = args.magma.override {
+                    cudatoolkit = args.cudatoolkit;
+                };
+            };
+        in
+            {
+                base-env-vars = (argsWithOverrides.writeText
+                    ("base-env-vars")
+                    (''
+                        # Keep track of project directory
+                        export PROJECT_DIR="$(pwd)"
 
-    # Libraries setup
-    [ -z LD_LIBRARY_PATH ] && export LD_LIBRARY_PATH=""
-    export LD_LIBRARY_PATH=${hdf5}:$LD_LIBRARY_PATH
-    export LD_LIBRARY_PATH=${mpi}/lib:$LD_LIBRARY_PATH
-    export LD_LIBRARY_PATH=${pkgs.python38}/lib:$LD_LIBRARY_PATH
-    export LD_LIBRARY_PATH=${pkgs.stdenv.cc.cc.lib}/lib:$LD_LIBRARY_PATH
-    export LD_LIBRARY_PATH=${pkgs.zlib}/lib:$LD_LIBRARY_PATH
+                        # Libraries setup
+                        [ -z LD_LIBRARY_PATH ] && export LD_LIBRARY_PATH=""
+                        export LD_LIBRARY_PATH="${argsWithOverrides.hdf5}:$LD_LIBRARY_PATH"
+                        export LD_LIBRARY_PATH="${argsWithOverrides.openmpi}/lib:$LD_LIBRARY_PATH"
+                        export LD_LIBRARY_PATH="${argsWithOverrides.python38}/lib:$LD_LIBRARY_PATH"
+                        export LD_LIBRARY_PATH="${argsWithOverrides.stdenv.cc.cc.lib}/lib:$LD_LIBRARY_PATH"
+                        export LD_LIBRARY_PATH="${argsWithOverrides.zlib}/lib:$LD_LIBRARY_PATH"
 
-    # CUDA and magma path
-    export LD_LIBRARY_PATH="${cudatoolkit}/lib:${cudnn}/lib:${magma}/lib:$LD_LIBRARY_PATH"
-  '';
+                        # CUDA and magma path
+                        export LD_LIBRARY_PATH="${argsWithOverrides.cudatoolkit}/lib:${argsWithOverrides.cudnn}/lib:${argsWithOverrides.magma}/lib:$LD_LIBRARY_PATH"
+                    '')
+                );
 
-  base-pip-requirements = pkgs.writeTextFile {
-    name = "requirements.txt";
-    text = (builtins.readFile ./requirements.txt);
-  };
-}
+                base-pip-requirements = argsWithOverrides.writeTextFile {
+                    name = "requirements.txt";
+                    text = (builtins.readFile ./requirements.txt);
+                };
+            }
